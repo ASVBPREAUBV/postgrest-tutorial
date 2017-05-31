e7090488e2898d6c51affa7d65b411d459c0a1b0
Paul Mayer <paul@paul-mayer.eu>
2017-06-01 00:11:46 +0200

    db grant update2

--- db/02_init_anonymous.sql
+++ db/02_init_anonymous.sql
@@ -1,4 +1,4 @@
-GRANT USAGE ON SCHEMA public TO anonymous;
+GRANT ALL ON SCHEMA public TO anonymous;
 CREATE ROLE anonymous;
 GRANT ALL ON TABLE post TO anonymous;
 GRANT ALL ON TABLE post_id_seq TO anonymous;
